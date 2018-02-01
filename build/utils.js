b5d58bca2138dbef5b390febae20193f02eee268
Karma Tsering <Karma.Tsering@bjss.com>
2018-02-01 10:54:21 +0000

    util file code tidy up: removed commented lines

--- build/utils.js
+++ build/utils.js
@@ -57,19 +57,6 @@ exports.cssLoaders = function (options) {
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
-    // postcss: generateLoaders(),
-    // less: generateLoaders('less'),
-    // sass: generateLoaders('sass', {
-    //   indentedSyntax: true
-    // }),
-    // scss: generateLoaders('sass').concat({
-    //   loader: 'sass-resources-loader',
-    //   options: {
-    //     resources: path.resolve(__dirname, '../src/style/_variables.scss')
-    //   }
-    // }),
-    // stylus: generateLoaders('stylus'),
-    // styl: generateLoaders('stylus')
   }
 }
 
