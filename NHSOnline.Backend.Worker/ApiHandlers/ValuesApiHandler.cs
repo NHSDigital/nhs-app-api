7975cd8bf4ca8497dacc71343d5dcabe3b320948
Matthew Smith <matthew.smith@bjss.com>
2018-02-04 14:52:17 +0000

    add codegen into build with basic hello world example

---
+++ NHSOnline.Backend.Worker/ApiHandlers/ValuesApiHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NHSOnline.Backend.Worker.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NHSOnline.Backend.Worker.ApiHandlers
+{
+    internal class ValuesApiHandler : IValuesApiHandler
+    {
+        private const string StubUrl = "http://nhsonline.stubs.emis.im1/api/values";
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public async Task<IActionResult> ValuesGet()
+        {
+            var stubResponse = await HttpClient.GetStringAsync(StubUrl);
+            var stubValues = JsonConvert.DeserializeObject<IEnumerable<string>>(stubResponse);
+
+            var values = stubValues
+                .Prepend("Backend Value 1 (Generated)")
+                .Append("Backend Value 2 (Generated)");
+
+            return new OkObjectResult(values);
+        }
+    }
+}
--- NHSOnline.Backend.Worker/Startup.cs
+++ NHSOnline.Backend.Worker/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using NHSOnline.Backend.Worker.ApiHandlers;
+using NHSOnline.Backend.Worker.Controllers;
 
 namespace NHSOnline.Backend.Worker
 {
@@ -23,6 +19,8 @@ namespace NHSOnline.Backend.Worker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IValuesApiHandler, ValuesApiHandler>();
+
             services.AddMvc();
         }
 
