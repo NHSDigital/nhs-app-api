05dc4421efd6240e8ab07a09bcbb9024b772977a
Matthew Smith <matthew.smith@bjss.com>
2018-02-05 16:45:52 +0000

    remove Vue logo and use small amount of NHS style in the basic view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="bodyDiv">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <p class="testClass">This is a test class</p>
-
-    <h2>Ecosystem</h2>
   </div>
 </template>
 
@@ -13,34 +9,18 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'NHS Online',
     };
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../style/nhso.scss";
-$primary-color:#333;
 
-.testClass{
-  background-color: $primary-color;
+h1 {
+  @include h1;
+  background-color: $nhs_blue;
 }
 
-h1, h2 {
-  @include screen_title;
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
