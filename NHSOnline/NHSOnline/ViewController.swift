b448b13f37329bd47bdb3c2b855b693b555e69ae
Konrad Glowczynski <K.Glowczynski@kainos.com>
2018-02-08 17:47:06 +0000

    hello world app

--- NHSOnline/NHSOnline/ViewController.swift
+++ NHSOnline/NHSOnline/ViewController.swift
@@ -15,30 +15,39 @@ class ViewController: UIViewController, UITabBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBar.itemSpacing = 20.0
         tabBar.delegate = self
-        let urlRequest = URLRequest(url: URL(string: "https://111.nhs.uk")!)
-        webView.load(urlRequest)
+        loadWebView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    public func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem){
+    public func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         switch item.tag {
         case 4:
             print("tab item 4")
         case 3:
-              print("tab item 3")
+            print("tab item 3")
         case 2:
-              print("tab item 2")
+            print("tab item 2")
         case 1:
-              print("tab item 1")
+            print("tab item 1")
         default:
-              print("tab item 0")
+            print("tab item 0")
         }
+    }
+    
+    private func loadWebView() {
+        let webViewEndpointUrl = Bundle.main.infoDictionary!["WebViewEndpointUrl"] as! String
+        let urlRequest = URLRequest(url: URL(string: webViewEndpointUrl)!)
+        self.webView.frame = self.view.bounds
+
+        webView.load(urlRequest)
     }
 }
 
