ae942c8b697dacbb6f77b564212184e4072a5468
Karma Tsering <Karma.Tsering@bjss.com>
2018-02-13 16:50:09 +0000

    code review channges

--- NHSOnline/NHSOnline/ViewController.swift
+++ NHSOnline/NHSOnline/ViewController.swift
@@ -1,45 +1,22 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITabBarDelegate {
+class ViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var tabBar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initializeTabBar()
-        loadWebView()
+        loadDefaultWebPage()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    public func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        switch item.tag {
-        case 4:
-            print("tab item 4")
-        case 3:
-            print("tab item 3")
-        case 2:
-            print("tab item 2")
-        case 1:
-            print("tab item 1")
-        default:
-            print("tab item 0")
-        }
-    }
-    
-    private func initializeTabBar() {
-        tabBar.delegate = self
-        tabBar.itemSpacing = 20.0
-    }
-    
-    private func loadWebView() {
-        let webViewEndpointUrl = Bundle.main.infoDictionary!["WebViewEndpointUrl"] as! String
+    private func loadDefaultWebPage() {
+        let webViewEndpointUrl = Bundle.main.infoDictionary!["BaseUrl"] as! String
         let urlRequest = URLRequest(url: URL(string: webViewEndpointUrl)!)
-        webView.frame = view.bounds
-
         webView.load(urlRequest)
     }
 }
