c84d2e597bfef12aebfe549087fa93db7b03f10f
Aleem Urrehman <khawajaa@kainos.com>
2018-02-27 14:52:45 +0000

    Integrate browserstack * Create new runner script for nightewatch:   * Script Looks at configurations to determine which environment to run on.   * Runs either locally or on browserstack depending on the chosen environment. * Update `package.json` to run new script when running `test-browser`.

---
+++ test/e2e/nightwatch.base.conf.js
@@ -0,0 +1,17 @@
+require('babel-register');
+require('nightwatch-cucumber')({
+    cucumberArgs: [
+        '--require', './test/e2e/page-models',
+        '--require', './test/e2e/steps',
+        '--require', './test/e2e/support',
+        '--format', 'json:test/e2e/reports/cucumber_report.json',
+        './test/e2e/features'],
+});
+
+// http://nightwatchjs.org/gettingstarted#settings-file
+const config = {
+    output_folder: 'test/e2e/reports',
+    test_settings: {},
+};
+
+module.exports = config;
