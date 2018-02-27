9279892a16f8691d8891590fc00d1cba78753180
Karma Tsering <karma.tsering@bjss.com>
2018-02-27 09:12:30 +0000

    integration of symptoms navigation menu

--- NHSOnline/NHSOnline/ViewController.swift
+++ NHSOnline/NHSOnline/ViewController.swift
@@ -1,22 +1,43 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITabBarDelegate {
+    let BASE_URL_BUNDLE_KEY = "BaseUrl"
+    let NHS111_URL_BUNDLE_KEY = "nhs111Url"
+    
+    let SYMPTOMS_TAG = 0
+    
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var tabBar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBar.delegate = self
+        
         loadDefaultWebPage()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case SYMPTOMS_TAG:
+            onSymptomsMenuSelected()
+        default:
+            loadDefaultWebPage()
+        }
+    }
+    
+    private func onSymptomsMenuSelected() {
+        let nhs111 = Bundle.main.infoDictionary![NHS111_URL_BUNDLE_KEY] as! String
+        loadPage(urlString: nhs111)
     }
     
     private func loadDefaultWebPage() {
-        let baseUrl = Bundle.main.infoDictionary!["BaseUrl"] as! String
-        let urlRequest = URLRequest(url: URL(string: baseUrl)!)
+        let baseUrl = Bundle.main.infoDictionary![BASE_URL_BUNDLE_KEY] as! String
+        loadPage(urlString: baseUrl)
+    }
+    
+    private func loadPage(urlString url:String) {
+        let urlRequest = URLRequest(url: URL(string: url)!)
         webView.load(urlRequest)
     }
 }
