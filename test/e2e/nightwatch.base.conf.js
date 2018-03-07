055cc6737a15c2fbce21d09e58dc79798fd5ad34
jaroslawd <jaroslawd@kainos.com>
2018-03-07 13:47:55 +0000

    Modify cucumber args.

--- test/e2e/nightwatch.base.conf.js
+++ test/e2e/nightwatch.base.conf.js
@@ -1,17 +1,15 @@
 require('babel-register');
 require('nightwatch-cucumber')({
-    cucumberArgs: [
-        '--require', './test/e2e/page-models',
-        '--require', './test/e2e/steps',
-        '--require', './test/e2e/support',
-        '--format', 'json:test/e2e/reports/cucumber_report.json',
-        './test/e2e/features'],
+  cucumberArgs: [
+    '--require', './test/e2e',
+    '--format', 'json:test/e2e/reports/cucumber_report.json',
+    './test/e2e/features'],
 });
 
 // http://nightwatchjs.org/gettingstarted#settings-file
 const config = {
-    output_folder: 'test/e2e/reports',
-    test_settings: {},
+  output_folder: 'test/e2e/reports',
+  test_settings: {},
 };
 
 module.exports = config;
