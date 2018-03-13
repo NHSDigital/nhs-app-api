5577b8636b648c4f96594e2815d7b6d6f72b71de
Matthew Smith <matthew.smith@bjss.com>
2018-03-13 16:54:59 +0000

    remove hello-world API Handler now that the real contract is in place

--- NHSOnline.Backend.Worker/Startup.cs
+++ NHSOnline.Backend.Worker/Startup.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NHSOnline.Backend.Worker.ApiHandlers;
-using NHSOnline.Backend.Worker.Controllers;
-using NHSOnline.Backend.Worker.Factories;
 
 namespace NHSOnline.Backend.Worker
 {
@@ -20,7 +17,6 @@ namespace NHSOnline.Backend.Worker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IValuesApiHandler>(new ValuesApiHandler(new Config(Configuration), new HttpClientFactory()));
             services.AddMvc();
         }
 
