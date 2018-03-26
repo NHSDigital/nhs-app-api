dcb557174b5a41b24691b327f5ace84491a3da4d
Karma Tsering <Karma.Tsering@bjss.com>
2018-03-26 20:27:56 +0100

    added nhs 111 unavailability styling

--- NHSOnline/NHSOnline/ContainerViewControllers/NHS111OnlineUnavailabilityViewController.swift
+++ NHSOnline/NHSOnline/ContainerViewControllers/NHS111OnlineUnavailabilityViewController.swift
@@ -3,11 +3,16 @@ import os.log
 
 class NHS111OnlineUnavailabilityViewController: UIViewController {
     
+    @IBOutlet weak var errorLabelWrapper: UIView!
     @IBAction func reloadPageTrigger(_ sender: Any) {
         
         let parentVC = self.parent as? HomeViewController
         let webViewController = parentVC?.childViewControllers.first as? WebViewController
 
         webViewController?.webView.loadPage(url: (parentVC?.nhs111Url)!)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        errorLabelWrapper.layer.addTopBorder(color: UIColor.red, thickness: 1)
     }
 }
