5b09355369c71fa0ede3a4735776688a0bd164cf
Karma Tsering <Karma.Tsering@bjss.com>
2018-03-26 20:25:45 +0100

    reload button now reloads nhs 111 page

--- NHSOnline/NHSOnline/ContainerViewControllers/NHS111OnlineUnavailabilityViewController.swift
+++ NHSOnline/NHSOnline/ContainerViewControllers/NHS111OnlineUnavailabilityViewController.swift
@@ -1,13 +1,13 @@
 import UIKit
 import os.log
 
-class NativeViewController: UIViewController {
+class NHS111OnlineUnavailabilityViewController: UIViewController {
     
     @IBAction func reloadPageTrigger(_ sender: Any) {
         
         let parentVC = self.parent as? HomeViewController
         let webViewController = parentVC?.childViewControllers.first as? WebViewController
 
-        webViewController?.webView.loadPage(url: (parentVC?.pageUrl)!)
+        webViewController?.webView.loadPage(url: (parentVC?.nhs111Url)!)
     }
 }
