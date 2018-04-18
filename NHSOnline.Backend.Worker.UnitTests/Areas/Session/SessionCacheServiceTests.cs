6fc85f954564cf42c63989c74dc9b9e84784b6bb
Ian Nelson <iann@kainos.com>
2018-04-18 09:32:22 +0100

    NHSO-457 store session state
    Updated unit test to verify that correct method is being invoked on Redis database.
    Also to test that the GUID returned is the same value used as the key to the Redis entry.

--- NHSOnline.Backend.Worker.UnitTests/Areas/Session/SessionCacheServiceTests.cs
+++ NHSOnline.Backend.Worker.UnitTests/Areas/Session/SessionCacheServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -19,8 +20,7 @@ namespace NHSOnline.Backend.Worker.UnitTests.Areas.Session
         public static void ClassInitialize(TestContext testContext)
         {
             _fixture = new Fixture()
-                .Customize(new AutoMoqCustomization())
-                .Customize(new ApiControllerAutoFixtureCustomization());
+                .Customize(new AutoMoqCustomization());
         }
 
         [TestMethod]
@@ -53,6 +53,48 @@ namespace NHSOnline.Backend.Worker.UnitTests.Areas.Session
             var result = await sut.CreateUserSession(userSession);
 
             result.Should().NotBeNullOrEmpty();
+        }
+
+        [TestMethod]
+        public async Task CreateUserSession_UserSessionIsStoredInRedis()
+        {
+            // Arrange
+            var userSession = _fixture.Create<UserSession>();
+            RedisValue userSessionJson = JsonConvert.SerializeObject(userSession);
+
+            string redisSessionKey = null;
+            var database = new Mock<IDatabase>();
+            database
+                .Setup(x =>
+                    x.StringSetAsync(
+                        It.IsAny<RedisKey>(),
+                        userSessionJson,
+                        TimeSpan.FromMinutes(20),
+                        When.Always,
+                        CommandFlags.None))
+                .Returns(Task.FromResult(true))
+                .Callback<RedisKey, RedisValue, TimeSpan?, When, CommandFlags>((key, value, expiry, when, flags) =>
+                {
+                    redisSessionKey = key;  // Store the guid key that was used as Redis session key
+                })
+                .Verifiable();
+
+            var connectionMultiplexer = new Mock<IConnectionMultiplexer>();
+            connectionMultiplexer.Setup(x => x.GetDatabase(-1, null)).Returns(database.Object);
+
+            var connectionMultiplexerFactory = new Mock<IConnectionMultiplexerFactory>();
+            connectionMultiplexerFactory.Setup(x => x.GetMultiplexer(ConnectionMultiplexerName.Session))
+                .Returns(connectionMultiplexer.Object);
+
+            var systemUnderTest = new SessionCacheService(connectionMultiplexerFactory.Object);
+            //var systemUnderTest = _fixture.Create<SessionCacheService>();
+
+            // Act
+            var result = await systemUnderTest.CreateUserSession(userSession);
+
+            // Assert
+            database.Verify();
+            result.Should().Be(redisSessionKey);
         }
     }
 }
