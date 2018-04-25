0f4437e2653b88ba3b28353f77d0d3a34d9bc473
mmiotk <m.miotk@kainos.com>
2018-04-25 11:34:42 +0100

    Adding build script. Adding kubernetes deployment. Squashed commit of the following:
    commit 8c601d70d77047652ca17a6dc39ab16e2f6a1184
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:24:51 2018 +0200
    
        Adding push script.
    
    commit 3c26bf7efa286209db235f204704f81659900ade
    Merge: dc9e919 8725f26
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:20:21 2018 +0200
    
        Merge branch 'feature/deploy_stubs_to_kubernetes' of ssh://git.nhschoices.net:8222/nhsonline/nhsonline-backendworker into feature/deploy_stubs_to_kubernetes
    
    commit dc9e91982e8919cfaa73d8f234826e34e3887498
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:19:49 2018 +0200
    
        Adding push command.
    
    commit 8725f2684a7e15c5a8563eb3bd406b042c34b5d5
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:10:30 2018 +0200
    
        Typo fix.
    
    commit e291e243807d609830586b65e4d5f1e4b7702e25
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:01:30 2018 +0200
    
        Adding build script. Adding kubernetes deployment.
    
    commit 919ecaff2b94656740578f4de3e6e43ac0a445f8
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:10:30 2018 +0200
    
        Typo fix.
    
    commit 8f11d137e95e5fbfccf9dbbd4bd7746960a18640
    Author: Michal Schott <schott.michal@gmail.com>
    Date:   Wed Apr 25 12:01:30 2018 +0200
    
        Adding build script. Adding kubernetes deployment.

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -x $(command -v docker) ];
+then
+  echo "Docker is missing.";
+  exit 1
+fi
+
+if [ -z "${DOCKER_REGISTRY}" ];
+then
+  echo "Missing DOCKER_REGISTRY variable."
+  exit 1
+fi
+
+docker build . -t ${DOCKER_REGISTRY}/nhsonline-backendworker:$(git rev-parse HEAD) -f NHSOnline.Backend.Worker/Dockerfile
+docker tag ${DOCKER_REGISTRY}/nhsonline-backendworker:$(git rev-parse HEAD) ${DOCKER_REGISTRY}/nhsonline-backendworker:latest
+
+docker build . -t ${DOCKER_REGISTRY}/nhsonline-stubs:$(git rev-parse HEAD) -f NHSOnline.Backend.Worker.Stubs/Dockerfile
+docker tag ${DOCKER_REGISTRY}/nhsonline-stubs:$(git rev-parse HEAD) ${DOCKER_REGISTRY}/nhsonline-stubs:latest
