7508c453591bac69d6c4c3a4fd29c5bb614c9e56
mmiotk <m.miotk@kainos.com>
2018-05-23 17:33:10 +0200

    [Ops] Migrating deployment k8s from ACS to AKS

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if [[ -z  $KUBECTL_CONFIG  ]];
-then
-	echo "error: KUBECTL_CONFIG variable is not set!"
-	exit 1
-fi
-
 cd /repo
 git-crypt unlock ~/.cicd.key
+az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
 mkdir -p ~/.kube
-echo $KUBECTL_CONFIG | base64 -d | zcat > ~/.kube/config
+az aks get-credentials -n $AKSCLUSTERNAME -g $AKSRESOURCEGROUP
 sed -i "s/latest/$(git rev-parse HEAD)/" kubernetes/deployment.yaml
 kubectl apply -f kubernetes/
