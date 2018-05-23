5487d936be3c7f1a6a3337b48642403672f64639
Dawid Jagiela <dawidj@kainos.com>
2018-05-23 17:37:29 +0200

    Cleaned up deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ -z  $KUBECTL_CONFIG  ]];
-then
-	echo "error: KUBECTL_CONFIG variable is not set!"
-	exit 1
-fi
-
 cd /repo
 git-crypt unlock ~/.cicd.key
 az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
