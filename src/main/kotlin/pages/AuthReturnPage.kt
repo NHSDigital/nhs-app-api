598c76afc43b4617e32977ac801b04d7628f9bdd
= <Jason.Davis@bjss.com>
2018-06-07 09:46:29 +0100

    Moved spinner detection to base class, and used the find using x-path as opposed to id.

--- src/main/kotlin/pages/AuthReturnPage.kt
+++ src/main/kotlin/pages/AuthReturnPage.kt
@@ -1,15 +1,7 @@
 package pageobjects
 
-import net.serenitybdd.core.annotations.findby.FindBy
 import pages.HybridPageObject
-import net.serenitybdd.core.pages.WebElementFacade
 
 class AuthReturnPage : HybridPageObject(Companion.PageType.WEBVIEW_APP) {
 
-        @FindBy(id = "loading-spinner")
-        lateinit var spinner: WebElementFacade
-
-        fun spinnerVisible(): Boolean {
-                return spinner.isCurrentlyVisible
-        }
 }
