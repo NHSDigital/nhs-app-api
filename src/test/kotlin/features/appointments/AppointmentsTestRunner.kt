ff733f38347aa8a2d7ed4c2a415aec3b37b57b24
jaroslawd <jaroslawd@kainos.com>
2018-06-08 11:42:39 +0100

    NHSO-642 run all appointments tests.

--- src/test/kotlin/features/appointments/AppointmentsTestRunner.kt
+++ src/test/kotlin/features/appointments/AppointmentsTestRunner.kt
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith
 import org.openqa.selenium.WebDriver
 
 @RunWith(CucumberWithSerenity::class)
-@CucumberOptions(features = ["src/test/kotlin/features/appointments/book_available_appointment_slot.feature:129"], glue = ["features"])
+@CucumberOptions(features = ["src/test/kotlin/features/appointments"], glue = ["features"])
 class AppointmentsTestRunner {
 
     @Managed
