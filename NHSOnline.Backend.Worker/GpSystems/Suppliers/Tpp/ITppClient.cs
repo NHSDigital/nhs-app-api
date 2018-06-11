9202630225673de2efa0fe6dca36cce75bd3adc9
Aaron Broughton-Janes <aaron.broughton-janes@bjss.com>
2018-06-11 14:06:32 +0100

    NHSO-124/NHSO-1228: Update `ITppClient` to return an `AuthenticateReply`
    (cherry picked from commit 53b2b211f978aed6bfda5619abc19eb7c8386e1d)

---
+++ NHSOnline.Backend.Worker/GpSystems/Suppliers/Tpp/ITppClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NHSOnline.Backend.Worker.GpSystems.Suppliers.Tpp.Models;
+
+namespace NHSOnline.Backend.Worker.GpSystems.Suppliers.Tpp
+{
+    public interface ITppClient
+    {
+        Task<AuthenticateReply> AuthenticatePost();
+    }
+}
