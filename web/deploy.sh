5d275d0596114f2939f99174eaead2b8c3b2854f
Dawid Jagiela <d.jagiela@kainos.com>
2018-08-08 12:21:51 +0100

    Change npm repository to point to nexus

--- web/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /repo
-git-crypt unlock ~/.cicd.key
-az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
-mkdir -p ~/.kube
-az aks get-credentials -n $AKSCLUSTERNAME -g $AKSRESOURCEGROUP
-sed -i "s/latest/$(git rev-parse HEAD)/" kubernetes/deployment.yaml
-kubectl apply -f kubernetes/
