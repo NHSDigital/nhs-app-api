663ba0fe29aa5922934d941b4d3b9258ea0fdc09
nathanaelh <nathanaelh@kainos.com>
2018-10-04 12:18:32 +0100

    NHSO-2668 prevent users from bypassing terms and conditions by manipulating the url

--- web/src/middleware/termsAndConditions.js
+++ web/src/middleware/termsAndConditions.js
@@ -1,9 +1,18 @@
+import { isAnonymous } from '@/lib/routes';
+
 export default async ({ redirect, route, store }) => {
-  if (route.name === 'terms-and-conditions') {
-    await store.dispatch('termsAndConditions/checkAcceptance');
-    if (store.state.termsAndConditions.areAccepted) {
+  if (isAnonymous(route.name)) {
+    return Promise.resolve();
+  }
+  await store.dispatch('termsAndConditions/checkAcceptance');
+  if (store.state.termsAndConditions.areAccepted) {
+    if (route.name === 'terms-and-conditions') {
       return redirect('/');
     }
+    return Promise.resolve();
   }
-  return Promise.resolve();
+  if (route.name === 'terms-and-conditions') {
+    return Promise.resolve();
+  }
+  return redirect('/terms-and-conditions');
 };
