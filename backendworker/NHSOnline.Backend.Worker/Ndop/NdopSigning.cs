3c8a28ea8419a5a0d7b26075feb78b3a5c46be1d
Stephanie McKay <stephanie.mckay@bjss.com>
2018-10-11 16:24:41 +0100

    NHSO-2831 Change NDOP to use certificate service

--- backendworker/NHSOnline.Backend.Worker/Ndop/NdopSigning.cs
+++ backendworker/NHSOnline.Backend.Worker/Ndop/NdopSigning.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using NHSOnline.Backend.Worker.Support.Certificate;
 
 namespace NHSOnline.Backend.Worker.Ndop
 {
     public class NdopSigning: INdopSigning
     {
         private readonly IConfiguration _configuration;
+        private readonly ICertificateService _certificateService;
         private readonly ILogger<NdopService> _logger;
         
-        public NdopSigning(IConfiguration configuration, ILoggerFactory loggerFactory)
+        public NdopSigning(IConfiguration configuration,
+            ILoggerFactory loggerFactory,
+            ICertificateService certificateService)
         {
             _configuration = configuration;
+            _certificateService = certificateService;
             _logger = loggerFactory.CreateLogger<NdopService>();
         }
         
@@ -24,15 +27,9 @@ namespace NHSOnline.Backend.Worker.Ndop
             {
                 var certPath = _configuration.GetOrWarn("NDOP_CERTIFICATE_PATH", _logger);
                 var password = _configuration.GetOrWarn("NDOP_CERTIFICATE_PASSWORD", _logger);
-
                 _logger.LogInformation("NDOP_CERTIFICATE_PATH: {path}", certPath);
-                if (string.IsNullOrEmpty(certPath) || !File.Exists(certPath) || string.IsNullOrEmpty(password))
-                {
-                    _logger.LogError("Could not get Ndop certificate due to missing certificate path or password.");
-                    return null;
-                }
-                
-                var certificate = new X509Certificate2(certPath, password);
+
+                var certificate = _certificateService.GetCertificate(certPath, password);
 
                 var key = new X509SecurityKey(certificate);
                 
