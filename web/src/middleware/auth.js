094bc50b3a5956402cb6d9eca98e660e82183827
Mark Lynn <markl@kainos.com>
2018-10-31 14:58:01 +0000

    NHSO 2896 Prevent going to login screen when already logged in

--- web/src/middleware/auth.js
+++ web/src/middleware/auth.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable import/extensions */
 import AuthorisationService from '@/services/authorisation-service';
-import { isAnonymous, BEGINLOGIN } from '@/lib/routes';
+import { isAnonymous, BEGINLOGIN, LOGIN } from '@/lib/routes';
 
 export default function ({ app, store, redirect, route }) {
-  if (!isAnonymous(route.name) && !store.getters['session/isLoggedIn']()) {
-    redirect('/login');
+  const isLoggedIn = store.getters['session/isLoggedIn']();
+
+  if (!isAnonymous(route.name) && !isLoggedIn) {
+    return redirect('/login');
+  }
+
+  if (route.name === LOGIN.name && isLoggedIn) {
+    return redirect('/');
   }
 
   if (route.name === BEGINLOGIN.name) {
@@ -15,6 +21,8 @@ export default function ({ app, store, redirect, route }) {
       store.$cookies,
     );
 
-    redirect(`${loginValues.authoriseUrl}?scope=${loginValues.scope}&client_id=nhs-online&code_challenge=${loginValues.codeChallenge}&code_challenge_method=${loginValues.codeChallengeMethod}&redirect_uri=${encodeURIComponent(loginValues.redirectUri)}&state=${loginValues.state}&response_type=${loginValues.responseType}`);
+    return redirect(`${loginValues.authoriseUrl}?scope=${loginValues.scope}&client_id=nhs-online&code_challenge=${loginValues.codeChallenge}&code_challenge_method=${loginValues.codeChallengeMethod}&redirect_uri=${encodeURIComponent(loginValues.redirectUri)}&state=${loginValues.state}&response_type=${loginValues.responseType}`);
   }
+
+  return undefined;
 }
