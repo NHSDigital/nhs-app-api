a86e58d536c5acace394e039c8ea591161b32a04
abrowne <aaron.browne@bjss.com>
2018-11-02 15:00:37 +0000

    nhso-2981 - Generate report for asp net core

--- backendworker/unit_tests.sh
+++ backendworker/unit_tests.sh
@@ -10,4 +10,11 @@ docker run \
   -v $HOST_PATH:$CONTAINER_PATH $DOCKER_IMAGE \
   /bin/bash -c "  \
   cd /repo ; \
-  dotnet test NHSOnline.Backend.Worker.UnitTests"
+  rm -rf NHSOnline.Backend.Worker.UnitTests/coverage ; \
+  apt-get update; \
+  apt-get install -y xsltproc; \
+  dotnet test /p:CoverletOutputFormat=opencover  /p:CollectCoverage=true /p:CoverletOutput=coverage/ NHSOnline.Backend.Worker.UnitTests ; \
+  dotnet ~/.nuget/packages/reportgenerator/4.0.2/tools/netcoreapp2.0/ReportGenerator.dll '-reports:NHSOnline.Backend.Worker.UnitTests/coverage/coverage.opencover.xml' '-targetdir:NHSOnline.Backend.Worker.UnitTests/coverage' ; \
+  mv NHSOnline.Backend.Worker.UnitTests/coverage/index.htm NHSOnline.Backend.Worker.UnitTests/coverage/index.html ; \
+  xsltproc NHSOnline.Backend.Worker.UnitTests/opencover_to_teamcity_msg.xsl NHSOnline.Backend.Worker.UnitTests/coverage/coverage.opencover.xml | grep 'teamcity' ; \
+"
