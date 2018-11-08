fb51081bc9d6df702fb21003f067c8bdd5996013
David Spence <d.spence@kainos.com>
2018-11-08 10:41:28 +0000

    Adding build variants for local dev (android and iOS)

--- web/run-with-https-android.sh
+++ web/run-with-https-android.sh
@@ -3,7 +3,7 @@
 certificate=~/.nhsonline/local-development-certificate/local-development-https.crt
 
 if [ -f $certificate ]; then
-  docker-compose -f docker-compose.yml -f docker-compose.https-android.yml up "$@"
+  docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.https-android.yml up "$@"
 else
   (>&2 echo ERROR: Could not find $certificate )
   (>&2 echo ERROR: Please run create-certificate.sh )
