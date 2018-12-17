15fb145a06e83a4df57173a70235109e7595bf9a
John Mckay <j.mckay@kainos.com>
2018-12-17 11:49:23 +0000

    NHSO-3591 Fixed brothermailer submit to not redirect on a 302 response, removed url from form

--- web/src/services/brother-mailer-service.js
+++ web/src/services/brother-mailer-service.js
@@ -1,18 +1,13 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable dot-notation */
 import axios from 'axios';
-import { GP_FINDER_SENDING_EMAIL_RESULT } from '@/lib/routes';
 
 export default {
-  postEmailToBrotherMailer: (appUrl, email, odscode) => {
-    const returnUrl = appUrl;
-    returnUrl.pathname = GP_FINDER_SENDING_EMAIL_RESULT.path;
-
+  postEmailToBrotherMailer: (email, odscode) => {
     const formContent =
       `userid=${process.env['GP_LOOKUP_BROTHER_MAILER_USER_ID']}&` +
       `${process.env['GP_LOOKUP_BROTHER_MAILER_SIG']}=&` +
       `addressbookid=${process.env['GP_LOOKUP_BROTHER_MAILER_ADDRESSBOOK_ID']}&` +
-      `ReturnURL=${returnUrl}&` +
       'ci_consenturl=&' +
       `email=${encodeURI(`${email}`)}&` +
       `cd_ODSCODE=${odscode}`;
@@ -25,8 +20,10 @@ export default {
     return axios({ url,
       crossDomain: true,
       withCredentials: true,
+      validateStatus: status => status === 302,
       method,
       headers,
+      maxRedirects: 0,
       data: formContent });
   },
 };
