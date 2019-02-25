c9716e963286c463fdcc2722aee468714f5de260
Karma <ktsering5@gmail.com>
2019-02-25 14:24:19 +0000

    [nhso-4567] fixed incorrect class data type in Web NHS Logo component

--- web/src/components/icons/HomeIcon.vue
+++ web/src/components/icons/HomeIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <tab-focused-icon :icon-classes="$style['nhs_logo']" :disable-tab-focus="true"
+  <tab-focused-icon :icon-classes="[$style['nhs_logo']]" :disable-tab-focus="true"
                     view-box="0 0 79 32" preserve-aspect-ratio="xMinYMid meet"
                     role="img" aria-hidden="true"
                     version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
