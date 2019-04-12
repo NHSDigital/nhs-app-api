71d908d3e9b9630d4944814a4b298ffc77a077d1
Conuir Williamson <conuirw@kainos.com>
2019-04-12 08:56:22 +0100

    NHSO-5195 Adding unit test for clearing throttling state

---
+++ web/test/unit/pages/gp-finder/pages.gp-finder.waiting-list-joined.spec.js
@@ -0,0 +1,19 @@
+import WaitingListJoinedPage from '@/pages/gp-finder/waiting-list-joined';
+import { mount, createStore } from '../../helpers';
+
+describe('Waiting List Joined/Not Joined page', () => {
+  it('will reset the throttling store when destroyed', () => {
+    const $store = createStore({
+      state: {
+        header: {
+          headerText: '',
+        },
+      },
+    });
+    mount(WaitingListJoinedPage, {
+      $store,
+    }).destroy();
+
+    expect($store.dispatch).toHaveBeenCalledWith('throttling/init');
+  });
+});
