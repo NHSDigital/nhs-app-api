32f9fcaa5d179d253269d377847238edafc6cdb4
Paul Sharpe <paul.sharpe@bjss.com>
2019-06-03 15:49:50 +0100

    NHSO-4508: Update SJR docker file to build single image

--- backendworker/ops/sjr_validation_build.sh
+++ backendworker/ops/sjr_validation_build.sh
@@ -5,9 +5,9 @@ set -e
 echo "Commit hash: $(git rev-parse HEAD)"
 echo "Commit hash: $(git rev-parse HEAD)" > version.txt
 
-IMAGE=nhsonline-backend-sjr-validation
+IMAGE=nhsonline-backendservicejourneyrulesapi
 DOCKER_REGISTRY=${DOCKER_REGISTRY:-nhsapp.azurecr.io}
 OUTPUT_FOLDER_PATH=/src/NHSOnline.Backend.ServiceJourneyRulesApi/configurations/output/
 
-docker build --pull --build-arg SJR_ARGS=--validate-only -t ${DOCKER_REGISTRY}/$IMAGE:$(git rev-parse HEAD) -f NHSOnline.Backend.ServiceJourneyRulesApi/Dockerfile .
-docker run -e "OUTPUT_FOLDER_PATH=$OUTPUT_FOLDER_PATH" -v $PWD/configurations/:$OUTPUT_FOLDER_PATH ${DOCKER_REGISTRY}/$IMAGE:$(git rev-parse HEAD)
+docker pull  ${DOCKER_REGISTRY}/$IMAGE:$(git rev-parse HEAD)
+docker run -e "SJR_ARGS=--validate-only" -e "OUTPUT_FOLDER_PATH=$OUTPUT_FOLDER_PATH" -v $PWD/configurations/:$OUTPUT_FOLDER_PATH ${DOCKER_REGISTRY}/$IMAGE:$(git rev-parse HEAD)
