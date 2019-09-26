13a47a4e97691a7ebce04440fdea3b246f591d8d
David Spence <d.spence@kainos.com>
2019-09-26 17:12:19 +0100

    NHSO-6895: add throttling results page unit test

--- web/test/unit/pages/gp-finder/results.spec.js
+++ web/test/unit/pages/gp-finder/results.spec.js
@@ -1,4 +1,5 @@
 import ResultsPage from '@/pages/gp-finder/results';
+import { mount } from '../../helpers';
 import '@babel/polyfill';
 
 describe(('GP Finder results page'), () => {
@@ -38,6 +39,76 @@ describe(('GP Finder results page'), () => {
       expect(actualInvalidOdsCode).toEqual(expectedInvalidOdsCode);
 
       expect(actualUndefinedOdsCode).toEqual(expectedUndefinedOdsCode);
+    });
+  });
+
+  describe(('results section'), () => {
+    const mountPage = ($store, data) => {
+      const page = mount(ResultsPage, {
+        $store,
+        data,
+      });
+      page.vm.goToUrl = jest.fn();
+      return page;
+    };
+
+    const createStore = () => ({
+      dispatch: jest.fn(),
+      state: {
+        device: {
+          source: 'Android',
+        },
+        throttling: {
+          searchResults: {
+            organisations: [
+              {
+                organisationName: 'org1',
+                nacsCode: 'abc',
+              },
+              {
+                organisationName: 'org2',
+                nacsCode: 'def',
+              },
+            ],
+            technicalError: false,
+            noResultsFound: false,
+            tooManyResults: false,
+          },
+          searchQuery: 'boom',
+        },
+      },
+    });
+
+    it('a user can click on a result which triggers gpPracticeClicked', () => {
+      // arrange
+      const $store = createStore();
+
+      // simulate what async data does and copy things from store to data
+      // so it's ready for mounted()
+      const { throttling } = $store.state;
+
+      const data = () => ({
+        technicalError: throttling.searchResults.technicalError,
+        noResultsFound: throttling.searchResults.noResultsFound,
+        tooManyResults: throttling.searchResults.tooManyResults,
+        organisations: throttling.searchResults.organisations,
+        searchQuery: throttling.searchQuery,
+      });
+
+      const wrapper = mountPage($store, data);
+
+      const organisationToClickOn = throttling.searchResults.organisations[0];
+
+      wrapper.vm.gpPracticeClicked = jest.fn();
+      const button = wrapper.find(`#btnGpPractice-${organisationToClickOn.nacsCode}`);
+      expect(button).toBeDefined();
+
+      // act
+      button.trigger('click');
+
+      // assert
+      expect(button.text()).toBe(organisationToClickOn.organisationName);
+      expect(wrapper.vm.gpPracticeClicked).toHaveBeenCalled();
     });
   });
 });
