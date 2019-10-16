5b57ee49a5a1b39b97b9244a00ca02a02e92d943
James Lavery <j.lavery@kainos.com>
2019-10-16 13:35:00 +0100

    NHSO-6901: Breadcrumb label incorrect on biometrics page iOS

--- ios/NHSOnline/NHSOnlineTests/BiometricBreadcrumbTests.swift
+++ ios/NHSOnline/NHSOnlineTests/BiometricBreadcrumbTests.swift
@@ -5,10 +5,8 @@ class BiometricBreadcrumTests: XCTestCase {
     let biometricBreadCrumb: BiometricBreadcrumb = BiometricBreadcrumb()
     
     func test_BiometricBreadcrumb_shows_MyAccountText() {
-        let accountLinktext = "Back to My Account"
+        let accountLinktext = "Back"
         biometricBreadCrumb.setupBreadcrumb()
         XCTAssertEqual(accountLinktext, biometricBreadCrumb.biometricLabel.attributedText?.string)
     }
 }
-
-
