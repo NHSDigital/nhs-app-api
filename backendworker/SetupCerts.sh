8953bc5efc3dc7dfcd8a0dd809ae07a8a43efb6e
Nikodem Jedynak <nikodemj@kainos.com>
2019-12-18 20:09:24 +0000

    Fixing broken self-signed certificates

--- backendworker/SetupCerts.sh
+++ backendworker/SetupCerts.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-echo "#! /bin/bash" > bootstrap.sh
-chmod 755 bootstrap.sh
-
 CERTS=0
 function copy_cert {
     if [ -f "$1" ]; then
@@ -26,8 +23,6 @@ copy_cert Suppliers/Spine/PublicKeys/nhsapp.ncrs.nhs.uk.crt
 copy_cert Suppliers/Spine/PublicKeys/subca-prod.crt
 copy_cert Suppliers/Spine/PublicKeys/root-prod.crt
 
-if [ $CERTS -ne 0 ]; then
-    chmod 644 /usr/local/share/ca-certificates/*
-    update-ca-certificates
-    echo "sudo update-ca-certificates" >> bootstrap.sh
-fi;
+chmod 755 /usr/local/share/ca-certificates/ 
+find /usr/local/share/ca-certificates/ -type f -print -exec chmod 644 {} \;
+update-ca-certificates
