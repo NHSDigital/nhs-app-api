079a5a5c992b8e340e04dafb27a90c53e15202c5
Adulai Sow <adulai.sow@bjss.com>
2020-02-13 10:55:53 +0000

    NHSO-8511: Underline links in error body content

--- web/src/components/errors/ErrorLink.vue
+++ web/src/components/errors/ErrorLink.vue
@@ -1,13 +1,12 @@
 <template>
-  <div v-if="isVisible" class="nhsuk-back-link nhsuk-u-margin-bottom-3">
-    <a class="nhsuk-back-link__link nhsuk-u-padding-left-0" :class="$style['error-link']"
-       :href="actionUrl"
+  <p v-if="isVisible">
+    <a :href="actionUrl"
        :target="target"
        data-purpose="main-back-button"
        @click.stop.prevent="onRetryButtonClicked">
       {{ $t(from) }}
     </a>
-  </div>
+  </p>
 </template>
 
 <script>
@@ -61,14 +60,3 @@ export default {
   },
 };
 </script>
-
-<style module lang="scss" scoped>
-@import '~nhsuk-frontend/packages/core/settings/typography';
-@import '~nhsuk-frontend/packages/core/tools/ifff';
-@import '~nhsuk-frontend/packages/core/tools/sass-mq';
-@import '~nhsuk-frontend/packages/core/tools/typography';
-
-.error-link {
-  @include nhsuk-typography-responsive(19);
-}
-</style>
