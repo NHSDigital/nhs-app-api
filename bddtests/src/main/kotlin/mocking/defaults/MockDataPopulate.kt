cc65aa62e9b5d298ddff28e328d5fe18efbdba88
Andrew White <andrew.white@bjss.com>
2020-04-06 11:38:55 +0100

    NHSO-9061: Set MOCK_NATIVE_LOGIN to false for stub mock data populate

--- bddtests/src/main/kotlin/mocking/defaults/MockDataPopulate.kt
+++ bddtests/src/main/kotlin/mocking/defaults/MockDataPopulate.kt
@@ -1,12 +1,15 @@
 package mocking.defaults
 
 import mocking.stubs.StubbedEnvironment
+import utils.GlobalSerenityHelpers
+import utils.set
 
 open class MockDataPopulate {
 
     companion object {
         @JvmStatic
         fun main(arguments: Array<String>) {
+            GlobalSerenityHelpers.MOCK_NATIVE_LOGIN.set(false)
             StubbedEnvironment().generateStubs()
         }
     }
