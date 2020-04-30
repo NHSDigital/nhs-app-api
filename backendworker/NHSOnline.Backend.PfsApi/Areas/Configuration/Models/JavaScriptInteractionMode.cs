7abb61ce50a31989ce9db77f2c39b3d0e1864137
Andrew Robinson <andrewro@kainos.com>
2020-04-30 13:49:18 +0100

    NHSO-8473: Silver Integration API - KnownServices updates

--- backendworker/NHSOnline.Backend.PfsApi/Areas/Configuration/Models/JavaScriptInteractionMode.cs
+++ backendworker/NHSOnline.Backend.PfsApi/Areas/Configuration/Models/JavaScriptInteractionMode.cs
@@ -3,6 +3,7 @@ namespace NHSOnline.Backend.PfsApi.Areas.Configuration.Models
     public enum JavaScriptInteractionMode
     {
         None,
-        NhsApp
+        NhsApp,
+        SilverThirdParty
     }
 }
