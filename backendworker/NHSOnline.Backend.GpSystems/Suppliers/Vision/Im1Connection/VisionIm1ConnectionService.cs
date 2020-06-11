ceebfe048b8cf122c8db9ec7452f9948a17a8497
Andrew White <andrew.white@bjss.com>
2020-06-11 17:34:57 +0100

    NHSO-10068: Reduce VisionIm1ConnectionService.Register class coupling

--- backendworker/NHSOnline.Backend.GpSystems/Suppliers/Vision/Im1Connection/VisionIm1ConnectionService.cs
+++ backendworker/NHSOnline.Backend.GpSystems/Suppliers/Vision/Im1Connection/VisionIm1ConnectionService.cs
@@ -79,73 +79,24 @@ namespace NHSOnline.Backend.GpSystems.Suppliers.Vision.Im1Connection
 
             try
             {
-                _logger.LogDebug("Checking cache for IM1 connection token");
-                var key = _im1CacheKeyGenerator.GenerateCacheKey(
-                    request.AccountId, request.OdsCode, request.LinkageKey);
-                var cachedConnectionToken =
-                    await _im1CacheService.GetIm1ConnectionToken<VisionConnectionToken>(key);
-
-                VisionConnectionToken connectionToken;
-
-                if (cachedConnectionToken.HasValue)
+                var getOrCreateIm1ConnectionTokenStep = await GetOrCreateIm1ConnectionToken(request);
+                if (getOrCreateIm1ConnectionTokenStep.ProcessFinishedEarly(out var getOrCreateFinalResult))
                 {
-                    connectionToken = cachedConnectionToken.ValueOrFailure();
-                    _logger.LogDebug("IM1 connection token found in cache.");
-                }
-                else
-                {
-                    var dob = request.DateOfBirth.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
-
-                    VisionPfsApiObjectResponse<ServiceContentRegisterResponse> linkAccountResponse;
-
-                    try
-                    {
-                        linkAccountResponse = await _visionClient.PostLinkAccount(request.OdsCode, request, dob);
-                    }
-                    catch (SocketException ex)
-                    {
-                        _logger.LogError(ex,
-                            $"Vision user with AccountId:{request.AccountId} throwing a Socket Exception.  Possibly already linked." +
-                            ex);
-                        return new Im1ConnectionRegisterResult.ErrorCase(Im1ConnectionErrorCodes.InternalCode
-                            .UserAlreadyLinked);
-                    }
-
-                    if (linkAccountResponse.HasErrorResponse)
-                    {
-                        return VisionIm1RegisterErrorMapper.Map(linkAccountResponse, _logger);
-                    }
-
-                    var apiToken = linkAccountResponse.Body.AuthenticationRef.ApiToken;
-                    connectionToken = new VisionConnectionToken
-                    {
-                        RosuAccountId = request.AccountId,
-                        ApiKey = apiToken,
-                        Im1CacheKey = key,
-                    };
-
-                    await _im1CacheService.SaveIm1ConnectionToken(key, connectionToken);
+                    return getOrCreateFinalResult;
                 }
 
-                var configResponse = await _visionClient.GetConfiguration(connectionToken, request.OdsCode);
-                if (configResponse.HasErrorResponse)
+                var connectionToken = getOrCreateIm1ConnectionTokenStep.Result;
+
+                var getConfigurationStep = await GetConfiguration(connectionToken, request.OdsCode);
+                if (getConfigurationStep.ProcessFinishedEarly(out var getConfigurationStepFinalResult))
                 {
-                    if (configResponse.FaultExists)
-                    {
-                        _logger.LogError("Error occurred when trying to obtain nhs number from get configuration. " +
-                                         $"Error: {configResponse.ErrorForLogging}.");
-                    }
-                    else
-                    {
-                        _logger.LogError("Error occurred when trying to obtain nhs number from get configuration. " +
-                                         $"Error Code: {configResponse.RawResponse.Body.VisionResponse.ServiceHeader.Outcome.Error.Code}. " +
-                                         $"Error description: {configResponse.RawResponse.Body.VisionResponse.ServiceHeader.Outcome.Error.Description}.");
-                        _logger.LogVisionErrorResponse(configResponse);
-                    }
-                    return new Im1ConnectionRegisterResult.BadGateway();
+                    return getConfigurationStepFinalResult;
                 }
+
+                var configResponse = getConfigurationStep.Result;
 
                 var response = CreatePatientIm1ConnectionResponse(request, configResponse, connectionToken);
+
                 _logger.LogInformation($"Vision returned {response.NhsNumbers?.Count()} NHS Numbers for the user");
                 _logger.LogDebug($"{nameof(VisionIm1ConnectionService)} Register successfully completed");
 
@@ -153,14 +104,120 @@ namespace NHSOnline.Backend.GpSystems.Suppliers.Vision.Im1Connection
             }
             catch (HttpRequestException e)
             {
-                _logger.LogCritical("Critical Error sending register request to vision");
-                _logger.LogCritical(e.ToString());
+                _logger.LogCritical(e, "Critical Error sending register request to vision");
                 return new Im1ConnectionRegisterResult.BadGateway();
             }
             finally
             {
                 _logger.LogExit();
             }
+        }
+
+        private async Task<ProcessResult<VisionConnectionToken, Im1ConnectionRegisterResult>> GetOrCreateIm1ConnectionToken(
+            PatientIm1ConnectionRequest request)
+        {
+            var getCachedStep = await GetCachedIm1ConnectionToken(request);
+            if (getCachedStep.ProcessFinishedEarly(out var cachedConnectionToken))
+            {
+                return ProcessResult.StepResult<VisionConnectionToken, Im1ConnectionRegisterResult>(
+                    cachedConnectionToken);
+            }
+
+            var cacheKey = getCachedStep.Result;
+
+            return await CreateIm1ConnectionToken(request, cacheKey);
+        }
+
+        private async Task<ProcessResult<string, VisionConnectionToken>> GetCachedIm1ConnectionToken(
+            PatientIm1ConnectionRequest request)
+        {
+            var cacheKey =
+                _im1CacheKeyGenerator.GenerateCacheKey(request.AccountId, request.OdsCode, request.LinkageKey);
+
+            _logger.LogDebug("Checking cache for IM1 connection token");
+            var cachedConnectionToken = await _im1CacheService.GetIm1ConnectionToken<VisionConnectionToken>(cacheKey);
+
+            return await cachedConnectionToken
+                .IfSome(connectionToken =>
+                {
+                    _logger.LogDebug("IM1 connection token found in cache.");
+                    var finalResult = ProcessResult.FinalResult<string, VisionConnectionToken>(connectionToken);
+                    return Task.FromResult(finalResult);
+                })
+                .IfNone(() =>
+                {
+                    var stepResult = ProcessResult.StepResult<string, VisionConnectionToken>(cacheKey);
+                    return Task.FromResult(stepResult);
+                });
+        }
+
+        private async Task<ProcessResult<VisionConnectionToken, Im1ConnectionRegisterResult>> CreateIm1ConnectionToken(
+            PatientIm1ConnectionRequest request,
+            string cacheKey)
+        {
+            var dob = request.DateOfBirth.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+            VisionPfsApiObjectResponse<ServiceContentRegisterResponse> linkAccountResponse;
+
+            try
+            {
+                linkAccountResponse = await _visionClient.PostLinkAccount(request.OdsCode, request, dob);
+            }
+            catch (SocketException ex)
+            {
+                _logger.LogError(ex,
+                    $"Vision user with AccountId:{request.AccountId} throwing a Socket Exception.  Possibly already linked.");
+                var finalResult =
+                    new Im1ConnectionRegisterResult.ErrorCase(
+                        Im1ConnectionErrorCodes.InternalCode.UserAlreadyLinked);
+                return ProcessResult.FinalResult<VisionConnectionToken, Im1ConnectionRegisterResult>(finalResult);
+            }
+
+            if (linkAccountResponse.HasErrorResponse)
+            {
+                var finalResult = VisionIm1RegisterErrorMapper.Map(linkAccountResponse, _logger);
+                return ProcessResult.FinalResult<VisionConnectionToken, Im1ConnectionRegisterResult>(finalResult);
+            }
+
+            var apiToken = linkAccountResponse.Body.AuthenticationRef.ApiToken;
+
+            var connectionToken = new VisionConnectionToken
+            {
+                RosuAccountId = request.AccountId,
+                ApiKey = apiToken,
+                Im1CacheKey = cacheKey,
+            };
+
+            await _im1CacheService.SaveIm1ConnectionToken(cacheKey, connectionToken);
+
+            return ProcessResult.StepResult<VisionConnectionToken, Im1ConnectionRegisterResult>(connectionToken);
+        }
+
+        private async Task<ProcessResult<VisionPfsApiObjectResponse<PatientConfigurationResponse>, Im1ConnectionRegisterResult>> GetConfiguration(
+            VisionConnectionToken connectionToken,
+            string odsCode)
+        {
+            var configResponse = await _visionClient.GetConfiguration(connectionToken, odsCode);
+            if (configResponse.HasErrorResponse)
+            {
+                if (configResponse.FaultExists)
+                {
+                    _logger.LogError("Error occurred when trying to obtain nhs number from get configuration. " +
+                                     $"Error: {configResponse.ErrorForLogging}.");
+                }
+                else
+                {
+                    _logger.LogError("Error occurred when trying to obtain nhs number from get configuration. " +
+                                     $"Error Code: {configResponse.RawResponse.Body.VisionResponse.ServiceHeader.Outcome.Error.Code}. " +
+                                     $"Error description: {configResponse.RawResponse.Body.VisionResponse.ServiceHeader.Outcome.Error.Description}.");
+                    _logger.LogVisionErrorResponse(configResponse);
+                }
+
+                var finalResult = new Im1ConnectionRegisterResult.BadGateway();
+                return ProcessResult.FinalResult<VisionPfsApiObjectResponse<PatientConfigurationResponse>, Im1ConnectionRegisterResult>(finalResult);
+            }
+
+            return ProcessResult.StepResult<VisionPfsApiObjectResponse<PatientConfigurationResponse>, Im1ConnectionRegisterResult>(configResponse);
         }
 
         private CreateIm1ConnectionResponse CreatePatientIm1ConnectionResponse(
