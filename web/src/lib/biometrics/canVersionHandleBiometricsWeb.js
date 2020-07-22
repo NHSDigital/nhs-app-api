7e7c72a03904c8dc42f5ac71dbefeef0c1289066
James Lavery <j.lavery@kainos.com>
2020-07-22 12:52:57 +0100

    NHSO-10680: Android biometrics not available

--- web/src/lib/biometrics/canVersionHandleBiometricsWeb.js
+++ web/src/lib/biometrics/canVersionHandleBiometricsWeb.js
@@ -1,5 +1,10 @@
 export default function (page) {
   const isNativeVersionAfter = page.$store.getters['appVersion/isNativeVersionAfter'];
+  const { device } = page.$store.state;
 
-  return !page.$store.state.device.isNativeApp || isNativeVersionAfter('1.34.0');
+  if (device.source === 'android') {
+    return isNativeVersionAfter('1.35.0');
+  }
+
+  return !device.isNativeApp || isNativeVersionAfter('1.34.0');
 }
