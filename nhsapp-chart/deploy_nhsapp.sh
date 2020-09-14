c599089299e6b0b9ae8ff95152c0062332538ae6
Benjamin Moss <benjaminm@kainos.com>
2020-09-14 13:15:54 +0100

    OPS-0523: CSI Driver for keyvault secrets

--- nhsapp-chart/deploy_nhsapp.sh
+++ nhsapp-chart/deploy_nhsapp.sh
@@ -91,36 +91,21 @@ else
 fi
 
 # Deploy Chart install
-# Environment secrets, e.g. dev, staging, production
-# Clone nhsapp-ops-vault repository
-info "Cloning nhsapp-ops-vault repo"
-### Azure DevOps repo code read-only token valid till: 7th Feb 2021 ixso7tesye5wk23eku5f2kopaacyin2ex3hjwwjavh4dkat5xcrq
-VAULT_REPO_PATH="../nhsapp-ops-vault"
-VAULT_REPO_URL="https://oauth2:ixso7tesye5wk23eku5f2kopaacyin2ex3hjwwjavh4dkat5xcrq@dev.azure.com/nhsapp/NHS%20App/_git/nhsapp-ops-vault"
-RELEASE_VAULT_REPO_TAG=${RELEASE_VAULT_REPO_TAG:-develop}
-git clone --single-branch --branch "$RELEASE_VAULT_REPO_TAG" "$VAULT_REPO_URL" "${VAULT_REPO_PATH}"
-
-# Unlock secrets repo
-pushd "${VAULT_REPO_PATH}"
-unlock_repo
-popd
-
 info "Checking for environment configuration files"
-[ -e "${VAULT_REPO_PATH}/vars/zone/$TARGET_ZONE/vars-$TARGET_ZONE.yaml" ] && {
+[ -e "../vars/zone/$TARGET_ZONE/vars-$TARGET_ZONE.yaml" ] && {
 	info "Zone override file found for $TARGET_ZONE, adding config";
-	CUSTOM_VALUES="-f ${VAULT_REPO_PATH}/vars/zone/$TARGET_ZONE/vars-$TARGET_ZONE.yaml";
+	CUSTOM_VALUES="-f ../vars/zone/$TARGET_ZONE/vars-$TARGET_ZONE.yaml";
 }
-[ -e "${VAULT_REPO_PATH}/vars/cluster/$AKS_SHORTNAME.yaml" ] && {
+[ -e "../vars/cluster/$AKS_SHORTNAME.yaml" ] && {
 	info "Cluster override found for $AKS_SHORTNAME, adding config";
-	CUSTOM_VALUES=$CUSTOM_VALUES" -f ${VAULT_REPO_PATH}/vars/cluster/vars-$AKS_SHORTNAME.yaml";
+	CUSTOM_VALUES=$CUSTOM_VALUES" -f ../vars/cluster/vars-$AKS_SHORTNAME.yaml";
 }
-[ -e "${VAULT_REPO_PATH}/vars/zone/$TARGET_ZONE/namespace/vars-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml" ] && {
+[ -e "$../vars/zone/$TARGET_ZONE/namespace/vars-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml" ] && {
 	info "Namespace override found for $TARGET_ZONE/$TARGET_ENVIRONMENT, adding config";
-	CUSTOM_VALUES=$CUSTOM_VALUES" -f ${VAULT_REPO_PATH}/vars/zone/$TARGET_ZONE/namespace/vars-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml";
+	CUSTOM_VALUES=$CUSTOM_VALUES" -f ../vars/zone/$TARGET_ZONE/namespace/vars-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml";
 }
 
 info "Adding values file params: ${CUSTOM_VALUES}"
-
 
 # Override stubbed environments if specified
 STUBBED_OVERRIDES=""
@@ -184,25 +169,8 @@ if ! kubectl get secrets -n "$TARGET_ENVIRONMENT" | grep -qm1 'nhsapp-service-nh
     [ "${TLS_CERT_SECRET_NAME}" != "" ] && kubectl get secret "$TLS_CERT_SECRET_NAME" --namespace=app-publishing --export -o yaml | kubectl apply --namespace="$TARGET_ENVIRONMENT" -f -
 fi
 
-info "Checking for $TARGET_ENVIRONMENT specific secrets"
-if [ -e "${VAULT_REPO_PATH}/secrets/zone/$TARGET_ZONE/namespace/secrets-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml" ]; then
-	info "Deploying $CHART-$appImageTag with $TARGET_ENVIRONMENT secrets"
-	CUSTOM_VALUES=$CUSTOM_VALUES" -f ${VAULT_REPO_PATH}/secrets/zone/$TARGET_ZONE/namespace/secrets-$TARGET_ZONE-$TARGET_ENVIRONMENT.yaml"
-else
-	info "Deploying $CHART-$appImageTag with no $TARGET_ENVIRONMENT override secrets"
-fi
-
-info "Checking for $REGION specific secrets"
-if [ "$TARGET_ZONE" == "production" ] || [ "$TARGET_ZONE" == "staging" ]; then
-	info "Deploying $CHART-$appImageTag with $REGION secrets"
-	CUSTOM_VALUES=$CUSTOM_VALUES" -f ${VAULT_REPO_PATH}/secrets/zone/$TARGET_ZONE/region/secrets-$TARGET_ZONE-$REGION.yaml"
-else
-	info "Deploying $CHART-$appImageTag with no $REGION override secrets"
-fi
-
 info "Beginning deployment -- $TARGET_ENVIRONMENT-$appImageTag..."
 helm upgrade "$TARGET_ENVIRONMENT-$appImageTag" \
-	-f "${VAULT_REPO_PATH}/secrets/zone/$TARGET_ZONE/secrets-$TARGET_ZONE.yaml" \
 	--set global.environment="$TARGET_ENVIRONMENT" \
 	--set appImageTag="$appImageTag" \
 	--set cid.image_tag="$CID_IMAGE_TAG" \
