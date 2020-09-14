fd7cd6f9a9017f50fedef7bc1276c7dc3fb1940a
Andrew White <andrew.white@bjss.com>
2020-09-14 16:16:38 +0100

    NHSO-10770: Add SJR health check to CID

--- backendworker/NHSOnline.Backend.CidApi/ServiceJourneyRules/ServiceConfigurationModule.cs
+++ backendworker/NHSOnline.Backend.CidApi/ServiceJourneyRules/ServiceConfigurationModule.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NHSOnline.Backend.HealthChecks;
 using NHSOnline.Backend.ServiceJourneyRules.Common;
 using NHSOnline.Backend.Support.Http;
 
 namespace NHSOnline.Backend.CidApi.ServiceJourneyRules
 {
-    public class ServiceConfigurationModule: NHSOnline.Backend.Support.DependencyInjection.ServiceConfigurationModule
+    public class ServiceConfigurationModule: Support.DependencyInjection.ServiceConfigurationModule
     {
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
@@ -17,7 +18,9 @@ namespace NHSOnline.Backend.CidApi.ServiceJourneyRules
             services.AddHttpClient<ServiceJourneyRulesHttpClient>()
                 .AddHttpMessageHandler<HttpTimeoutHandler<ServiceJourneyRulesHttpRequestIdentifier>>()
                 .AddHttpMessageHandler<HttpRequestIdentificationHandler<ServiceJourneyRulesHttpRequestIdentifier>>();
-            
+
+            services.AddNhsAppHealthCheck<ServiceJourneyRulesHttpClient>("SJR");
+
             base.ConfigureServices(services, configuration);
         }
     }
