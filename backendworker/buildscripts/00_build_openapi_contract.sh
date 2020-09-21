9e6459e247cdccb638cbe939e35d3309f11c0ab9
Andrew White <andrew.white@bjss.com>
2020-09-21 09:52:15 +0100

    NHSO-10975: Run bundleContracts.sh in docker container during backend build

--- backendworker/buildscripts/00_build_openapi_contract.sh
+++ backendworker/buildscripts/00_build_openapi_contract.sh
@@ -4,4 +4,15 @@ set -e
 # Change current working directory to be the root of backendworker, regardless of how this script is invoked
 cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
-./contracts/bundleContracts.sh
+# shellcheck source=../../buildscripts/lib/set_env.sh
+source "../buildscripts/lib/set_env.sh"
+
+DOCKER_ARGS=()
+DOCKER_ARGS+=(--rm)
+DOCKER_ARGS+=(-v "$REPO_ROOT/:/data/")
+DOCKER_ARGS+=(-v "${NPMRC_PATH}:/root/.npmrc")
+
+docker run \
+  "${DOCKER_ARGS[@]}" \
+  nhsapp.azurecr.io/nhsonline-nodejs-build:node14-1.0 \
+  ./backendworker/contracts/bundleContracts.sh
