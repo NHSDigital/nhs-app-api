af31b88945b36ad0f37f918a60dbdaebf4f23912
Andrew Robinson <a.robinson@kainos.com>
2021-01-20 10:15:05 +0000

    NHSO-12755: iOS app set NSAllowsArbitraryLoads to false

---
+++ ios/NHSOnline/BuildScripts/NHSOnline.sh
@@ -0,0 +1,16 @@
+INFO_PLIST="${SRCROOT}/${INFOPLIST_FILE}"
+PLISTBUDDY="/usr/libexec/PlistBuddy"
+
+if  [ "${CONFIGURATION}" = "Development" ] || [ "${CONFIGURATION}" = "Debug" ] || [ "${CONFIGURATION}" = "BrowserstackLocal" ]; then
+    $PLISTBUDDY -c "Set :NSAppTransportSecurity:NSAllowsArbitraryLoads true" "${INFO_PLIST}"
+else
+    $PLISTBUDDY -c "Set :NSAppTransportSecurity:NSAllowsArbitraryLoads false" "${INFO_PLIST}"
+fi
+
+ALLOWS_ARBITRARY_LOADS_ENABLED=$($PLISTBUDDY -c "Print :NSAppTransportSecurity:NSAllowsArbitraryLoads" "${INFO_PLIST}")
+
+if [ "${ALLOWS_ARBITRARY_LOADS_ENABLED}" == "true" ]; then
+    echo "warning: ATS is disabled. :NSAppTransportSecurity:NSAllowsArbitraryLoads set to true. This is insecure and should not be used in release builds."
+else
+    echo "information: ATS is enabled. :NSAppTransportSecurity:NSAllowsArbitraryLoads set to false"
+fi
