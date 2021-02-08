84b6ca18775717aea7c9ba111ad0b60d2dd10e60
Andrew White <andrew.white@bjss.com>
2021-02-08 20:04:23 +0000

    NHSO-12929: Update BrowserStack Appium version

--- xamarinintegrationtests/NHSOnline.IntegrationTests.UI/Drivers/Native/BrowserStackConfig.cs
+++ xamarinintegrationtests/NHSOnline.IntegrationTests.UI/Drivers/Native/BrowserStackConfig.cs
@@ -10,7 +10,7 @@ namespace NHSOnline.IntegrationTests.UI.Drivers.Native
         public string? User { get; set; } = "ops20";
         public string? Key { get; set; }
         public string? LocalIdentifier { get; set; } = $"int_test_{Dns.GetHostName()}";
-        public string AppiumVersion { get; set; } = "1.17.0";
+        public string AppiumVersion { get; set; } = "1.19.1";
         public string Project { get; set; } = "NHSApp";
         public string Build { get; set; } = $"{Dns.GetHostName()}-local";
         public bool EnableNetworkLogs { get; set; } = false;
