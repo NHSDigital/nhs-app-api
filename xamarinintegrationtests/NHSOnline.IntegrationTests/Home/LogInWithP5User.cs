a9fc4673bb69e8a7a8ff833fd774e26e9c332736
Rachel Kennedy <rachel.kennedy7@nhs.net>
2021-02-22 12:12:53 +0000

    NHSO-13301: P5 user can successfully log in

---
+++ xamarinintegrationtests/NHSOnline.IntegrationTests/Home/LogInWithP5User.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHSOnline.HttpMocks.Domain;
+using NHSOnline.IntegrationTests.Pages.Android.Home;
+using NHSOnline.IntegrationTests.Pages.Android.LoggedOut;
+using NHSOnline.IntegrationTests.Pages.IOS.Home;
+using NHSOnline.IntegrationTests.Pages.IOS.LoggedOut;
+using NHSOnline.IntegrationTests.UI;
+using NHSOnline.IntegrationTests.UI.Drivers;
+
+namespace NHSOnline.IntegrationTests.Home
+{
+    [TestClass]
+    [BusinessRule("BR-LOG-5.1", "Log in for a P5 user logs them into the app")]
+    public class LogInWithP5User
+    {
+        [NhsAppAndroidTest]
+        public void APatientWithProofLevelFiveCanSuccessfullyLogInAndroid(IAndroidDriverWrapper driver)
+        {
+            var patient = new P5Patient()
+                .WithName(b => b.GivenName("Terry").FamilyName("Tibbs"));
+            using var patients = Mocks.Patients.Add(patient);
+
+            AndroidLoggedOutHomePage
+                .AssertOnPage(driver)
+                .ContinueWithNhsLogin();
+
+            AndroidBeforeYouStartPage
+                .AssertOnPage(driver)
+                .Continue();
+
+            AndroidStubbedLoginPage
+                .AssertOnPage(driver)
+                .PageContent.Login(patient);
+
+            AndroidTermsAndConditionsPage
+                .AssertOnPage(driver)
+                .PageContent.AcceptTermsAndConditions();
+
+            AndroidUserResearchOptInPage
+                .AssertOnPage(driver)
+                .PageContent.OptInToUserResearch();
+
+            AndroidManageNotificationsPromptPage
+                .AssertOnPage(driver)
+                .PageContent.Continue();
+
+            AndroidLoggedInHomePage
+                .AssertOnPage(driver)
+                .AssertPageDisplayedFor("Terry Tibbs");
+        }
+
+        [NhsAppIOSTest]
+        public void APatientWithProofLevelFiveCanSuccessfullyLogInIos(IIOSDriverWrapper driver)
+        {
+            var patient = new P5Patient()
+                .WithName(b => b.GivenName("Terry").FamilyName("Tibbs"));
+            using var patients = Mocks.Patients.Add(patient);
+
+            IOSLoggedOutHomePage
+                .AssertOnPage(driver)
+                .ContinueWithNhsLogin();
+
+            IOSBeforeYouStartPage
+                .AssertOnPage(driver)
+                .Continue();
+
+            IOSStubbedLoginPage
+                .AssertOnPage(driver)
+                .PageContent.Login(patient);
+
+            IOSTermsAndConditionsPage
+                .AssertOnPage(driver)
+                .PageContent.AcceptTermsAndConditions();
+
+            IOSUserResearchOptInPage
+                .AssertOnPage(driver)
+                .PageContent.OptInToUserResearch();
+
+            IOSManageNotificationsPromptPage
+                .AssertOnPage(driver)
+                .PageContent.Continue();
+
+            IOSLoggedInHomePage
+                .AssertOnPage(driver)
+                .AssertPageDisplayedFor("Terry Tibbs");
+        }
+    }
+}
