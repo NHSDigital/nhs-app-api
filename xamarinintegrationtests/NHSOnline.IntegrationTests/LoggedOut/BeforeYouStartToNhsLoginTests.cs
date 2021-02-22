9c82c10beca13f2df53df8e18bc1f68d199e3eca
Rachel Kennedy <rachel.kennedy7@nhs.net>
2021-02-22 10:27:58 +0000

    NHSO-13315: Add before you start to nhs login tests

---
+++ xamarinintegrationtests/NHSOnline.IntegrationTests/LoggedOut/BeforeYouStartToNhsLoginTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHSOnline.IntegrationTests.Pages.Android.LoggedOut;
+using NHSOnline.IntegrationTests.Pages.IOS.LoggedOut;
+using NHSOnline.IntegrationTests.UI;
+using NHSOnline.IntegrationTests.UI.Drivers;
+
+namespace NHSOnline.IntegrationTests.LoggedOut
+{
+    [TestClass]
+    [BusinessRule("BR-LOG-2.2","Acknowledging the before you start screen displays NHS Login")]
+    public class BeforeYouStartToNhsLoginTests
+    {
+        [NhsAppAndroidTest]
+        public void APatientSeesTheNhsLoginJourneyAfterContinuingFromBeforeYouStartAndroid(IAndroidDriverWrapper driver)
+        {
+            AndroidLoggedOutHomePage
+                .AssertOnPage(driver)
+                .ContinueWithNhsLogin();
+
+            AndroidBeforeYouStartPage
+                .AssertOnPage(driver)
+                .Continue();
+
+            AndroidStubbedLoginPage
+                .AssertOnPage(driver);
+        }
+
+        [NhsAppIOSTest]
+        public void APatientSeesTheNhsLoginJourneyAfterContinuingFromBeforeYouStartIos(IIOSDriverWrapper driver)
+        {
+            IOSLoggedOutHomePage
+                .AssertOnPage(driver)
+                .ContinueWithNhsLogin();
+
+            IOSBeforeYouStartPage
+                .AssertOnPage(driver)
+                .Continue();
+
+            IOSStubbedLoginPage
+                .AssertOnPage(driver);
+        }
+    }
+}
