2f469daf5ff3a6c43068ea118375fb74fb9746e0
Conuir Williamson <conuirw@kainos.com>
2021-03-11 15:34:25 +0000

    NHSO-13437: Manual test for iProov verification iOS

---
+++ xamarinintegrationtests/NHSOnline.IntegrationTests/WebIntegration/NhsLoginUpliftIProovVerificationTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHSOnline.IntegrationTests.UI;
+
+namespace NHSOnline.IntegrationTests.WebIntegration
+{
+    [TestClass]
+    [BusinessRule("BR-LOG-10.1", "Capturing a face scan for identity verification in the PYI journey invokes iProov when iProov is enabled")]
+    public class NhsLoginUpliftIProovVerificationTests
+    {
+        [NhsAppManualTest("NHSO-13695", "No reliable means via BrowserStack to determine the SDK is available, at the moment")]
+        public void APatientWithProofLevelFiveWhenUpliftingCanVerifyTheirIdentityUsingIProovIOS() { }
+    }
+}
