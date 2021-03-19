e421c051815d5ec2cab29a46a376ac023a1578f7
Liam Mullen <liamm@kainos.com>
2021-03-19 12:27:26 +0000

    NHSO-13312: Mobile users can now zoom in on mobile browsers

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,6 +7,19 @@
 <script>
 export default {
   name: 'App',
+  mounted() {
+    this.setScaleValues();
+  },
+  methods: {
+    setScaleValues() {
+      const viewport = document.getElementsByName('viewport')[0];
+      let content = 'width=device-width, initial-scale=1, minimum-scale=1.0';
+      if (navigator.userAgent.indexOf('nhsapp-android') !== -1 || navigator.userAgent.indexOf('nhsapp-ios') !== -1) {
+        content += ', maximum-scale=1.0, user-scalable=0';
+      }
+      viewport.setAttribute('content', content);
+    },
+  },
 };
 </script>
 
