c9dd804690251abb56e3923558deb708398775b6
Thomas McDonnell <thomas.mcdonnell1@nhs.net>
2021-05-17 13:50:44 +0100

    NHSO-14151: Increasing worker count

--- xamarinintegrationtests/NHSOnline.IntegrationTests/Properties/AssemblyInfo.cs
+++ xamarinintegrationtests/NHSOnline.IntegrationTests/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: CLSCompliant(false)]
-[assembly: Parallelize(Workers = 3, Scope = ExecutionScope.MethodLevel)]
+[assembly: Parallelize(Workers = 7, Scope = ExecutionScope.MethodLevel)]
