9446cca571e9cbc67556aff81fe3edb14294bc6d
Andrew Robinson <a.robinson@kainos.com>
2021-06-15 10:20:44 +0100

    NHSO-15039: Adding devops known services JSON file validation

--- backendworker/buildscripts/01_validate_known_services.sh
+++ backendworker/buildscripts/01_validate_known_services.sh
@@ -7,6 +7,22 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
 alias recursiveSearch='grep --recursive --only-matching --extended-regexp --no-filename'
 alias nonRecursiveSearch='grep --only-matching --extended-regexp --no-filename'
+alias invertMatchSearch='grep --invert-match'
+
+function validate_json_files() {
+  nonRecursiveSearch 'http(s)?://.*' NHSOnline.Backend.PfsApi/KnownServicesV3.json | cut -d '"' -f 1 | invertMatchSearch 'https://WebAppBaseUrl' | sort - > servicesv3json.tmp
+  
+  nonRecursiveSearch 'http(s)?://.*' NHSOnline.Backend.PfsApi/KnownServices.json | cut -d '"' -f 1 | invertMatchSearch 'https://WebAppBaseUrl' | sort - > serviceslegacyjson.tmp
+
+  if diff serviceslegacyjson.tmp servicesv3json.tmp; then
+    echo "Legacy and v3 Known Services JSON files match"
+  else
+    echo "The legacy and v3 Known Services JSON files are different. See above about for details"
+    echo "< = url is present in Legacy known services, but missing in V3 known services"
+    echo "> = url is present in V3 known services, but missing in Legacy known services"
+    exit 1
+  fi
+}
 
 function validate_chart_files() {
   recursiveSearch 'servicesV3__.(.*http.*)' ../nhsapp-chart/vars | nonRecursiveSearch 'http.*' | sort - > servicesv3chart.tmp
@@ -24,9 +40,9 @@ function validate_chart_files() {
 }
 
 function validate_docker_env_files() {
-  recursiveSearch 'servicesV3__.(.*http.*)' ../docker/*.env | nonRecursiveSearch 'http.*' | sort -> servicesv3docker.tmp
+  recursiveSearch 'servicesV3__.(.*http.*)' ../docker/*.env | nonRecursiveSearch 'http.*' | sort - > servicesv3docker.tmp
   
-  recursiveSearch 'services__.(.*http.*)' ../docker/*.env | nonRecursiveSearch 'http.*' | sort -> serviceslegacydocker.tmp
+  recursiveSearch 'services__.(.*http.*)' ../docker/*.env | nonRecursiveSearch 'http.*' | sort - > serviceslegacydocker.tmp
 
   if diff serviceslegacydocker.tmp servicesv3docker.tmp; then
     echo "Legacy and v3 Known Services docker env match"
@@ -40,6 +56,7 @@ function validate_docker_env_files() {
 
 function main() {
   echo "Validating known services"
+  validate_json_files
   validate_chart_files
   validate_docker_env_files
 }
