f75d2362378047929838186b63f914be2445b5d6
Danielle Sherlock <danielle.sherlock@nhs.net>
2021-08-12 12:25:12 +0100

    NHSO-13739: Link Header Domain

---
+++ proxies/live/apiproxy/resources/jsc/rewrite-link-header.js
@@ -0,0 +1,12 @@
+var requestUrl = context.getVariable('request-url');
+var pathSplitter = context.getVariable('request-pathSuffix');
+var regexString = new RegExp('(?<=<)(.*?)(?:'+pathSplitter+')', 'g');
+var linkHeader = context.targetResponse.headers['Link'];
+
+var responseLink = linkHeader.replace(regexString, requestUrl);
+
+context.targetResponse.headers['Link'] = responseLink;
+
+
+
+
