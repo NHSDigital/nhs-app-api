0184b4cf9bcd57aa9699f8c04adad2f6d83985e8
James Lavery <j.lavery@kainos.com>
2021-09-01 11:07:35 +0100

    NHSO-15757: Changing logged out contextual help URL

--- xamarin/NHSOnline.App.Config/Values/Preview/PreviewNhsExternalServicesConfiguration.cs
+++ xamarin/NHSOnline.App.Config/Values/Preview/PreviewNhsExternalServicesConfiguration.cs
@@ -7,7 +7,7 @@ namespace NHSOnline.App.Config.Values.Preview
         public Uri NhsUkCovidUrl { get; } = new Uri("https://111.nhs.uk/service/COVID-19/");
         public Uri NhsUkCovidConditionsUrl { get; } = new Uri("https://www.nhs.uk/conditions/coronavirus-covid-19/");
         public Uri NhsUkBaseHelpUrl { get; } = new Uri("https://www.nhs.uk/using-the-nhs/nhs-services/the-nhs-app/help/");
-        public Uri NhsUkLoginHelpUrl { get; } = new Uri("https://www.nhs.uk/nhs-app/nhs-app-help-and-support/getting-started-with-the-nhs-app/logging-in-to-the-nhs-app/");
+        public Uri NhsUkLoginHelpUrl { get; } = new Uri("https://www.nhs.uk/nhs-app/nhs-app-help-and-support/getting-started-with-the-nhs-app/");
         public Uri NhsUkTechnicalIssuesHelpUrl { get; } = new Uri("https://www.nhs.uk/nhs-app/nhs-app-help-and-support/nhs-app-technical-information/technical-issues-with-the-nhs-app/");
         public Uri NhsUkConditionsUrl { get; } = new Uri("https://www.nhs.uk/conditions/");
         public Uri OneOneOneUrl { get; } = new Uri("https://111.nhs.uk/");
