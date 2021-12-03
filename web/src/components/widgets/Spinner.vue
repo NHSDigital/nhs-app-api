f089e93bf6d25ac5c8a5acc031b724f72893c8ac
Paul Sharpe <paul.sharpe@bjss.com>
2021-12-03 10:47:15 +0000

    NHSO-18180: Web spinner announce

--- web/src/components/widgets/Spinner.vue
+++ web/src/components/widgets/Spinner.vue
@@ -1,7 +1,16 @@
 <template>
-  <!--eslint-disable vue/html-self-closing -->
-  <div v-if="isVisible" class="loading-spinner-background" tabindex="-1" focusable="false">
-    <div class="loading-spinner" tabindex="-1" focusable="false"></div>
+  <div v-if="isVisible"
+       class="loading-spinner-background"
+       tabindex="-1"
+       focusable="false">
+    <div class="loading-spinner"
+         tabindex="-1"
+         focusable="false">
+      <span class="sr-only"
+            aria-live="polite">
+        {{ spinnerText }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -19,8 +28,11 @@ export default {
     isVisible() {
       return this.alwaysShow || (!this.$store.state.spinner.prevent &&
         (this.$store.getters['http/isLoading']
-        || this.$store.state.onlineConsultations.isLoadingFile
-        || this.$store.state.http.isLoadingExternalSite));
+          || this.$store.state.onlineConsultations.isLoadingFile
+          || this.$store.state.http.isLoadingExternalSite));
+    },
+    spinnerText() {
+      return this.isVisible ? 'Loading' : '';
     },
   },
 };
@@ -28,4 +40,5 @@ export default {
 
 <style lang="scss" scoped>
   @import "@/style/custom/spinner";
+  @import "../../style/accessibility";
 </style>
