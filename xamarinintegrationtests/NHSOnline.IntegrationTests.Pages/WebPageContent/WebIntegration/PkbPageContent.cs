bb701c90d3c312b7da82b2e9614a855afd8b15cd
Thomas McDonnell <thomas.mcdonnell1@nhs.net>
2021-12-14 15:40:05 +0000

    NHSO-0000: Fixing broken navigation test for missing browser overlay link

--- xamarinintegrationtests/NHSOnline.IntegrationTests.Pages/WebPageContent/WebIntegration/PkbPageContent.cs
+++ xamarinintegrationtests/NHSOnline.IntegrationTests.Pages/WebPageContent/WebIntegration/PkbPageContent.cs
@@ -36,6 +36,7 @@ namespace NHSOnline.IntegrationTests.Pages.WebPageContent.WebIntegration
         {
             CalendarLink,
             GoToPageLink,
+            OpenBrowserOverlayLink,
             FileUploadLink,
             DocumentDownloadLink,
             LocationServicesLink
