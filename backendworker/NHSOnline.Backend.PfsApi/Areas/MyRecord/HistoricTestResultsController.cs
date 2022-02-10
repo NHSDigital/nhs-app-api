53d273ab42ce5b2d3d5816e3204231d3957d8d4f
Jack McKenna <jackmc@kainos.com>
2022-02-10 11:31:08 +0000

    NHSO-18255: TPP test results endpoint authentication

--- backendworker/NHSOnline.Backend.PfsApi/Areas/MyRecord/HistoricTestResultsController.cs
+++ backendworker/NHSOnline.Backend.PfsApi/Areas/MyRecord/HistoricTestResultsController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NHSOnline.Backend.Auditing;
@@ -14,6 +16,7 @@ using static NHSOnline.Backend.Support.Constants.HttpHeaders;
 
 namespace NHSOnline.Backend.PfsApi.Areas.MyRecord
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class HistoricTestResultsController : Controller
     {
         private readonly IGpSystemFactory _gpSystemFactory;
