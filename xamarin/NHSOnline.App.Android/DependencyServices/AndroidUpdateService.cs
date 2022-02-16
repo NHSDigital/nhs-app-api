9e46dc22175a9478e947f988006020a15010b0d4
Jack Reeve <jack.reeve1@nhs.net>
2022-02-16 09:22:03 +0000

    NHSO-99999: Reverts changes from 18483

--- xamarin/NHSOnline.App.Android/DependencyServices/AndroidUpdateService.cs
+++ xamarin/NHSOnline.App.Android/DependencyServices/AndroidUpdateService.cs
@@ -1,10 +1,8 @@
 using System.Threading.Tasks;
 using Android.Content;
 using Android.Net;
-using Microsoft.Extensions.Logging;
 using NHSOnline.App.DependencyServices;
 using NHSOnline.App.Droid.DependencyServices;
-using NHSOnline.App.Logging;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(AndroidUpdateService))]
@@ -12,44 +10,16 @@ namespace NHSOnline.App.Droid.DependencyServices
 {
     public class AndroidUpdateService : IUpdateService
     {
-        private static ILogger Logger => NhsAppLogging.CreateLogger<AndroidUpdateService>();
-
         public Task OpenAppStoreUrl()
         {
-            var storeIntentResult = OpenIntent("market://details?id=com.nhs.online.nhsonline");
-            if (storeIntentResult == null)
-            {
-                return Task.CompletedTask;
-            }
+            using var openAppStoreIntent = new Intent(Intent.ActionView);
 
-            var browserIntentResult = OpenIntent("https://play.google.com/store/apps/details?id=com.nhs.online.nhsonline");
-            if (browserIntentResult == null)
-            {
-                Logger.LogError("Failed to open play store intent. {ErrorMessage}", storeIntentResult.Message);
-            }
-            else
-            {
-                Logger.LogError("Failed to open play store and browser intents. {ErrorMessage}\n{AdditionalErrorMessage}",
-                    storeIntentResult.Message, browserIntentResult.Message);
-            }
+            openAppStoreIntent.SetData(Uri.Parse("market://details?id=com.nhs.online.nhsonline"));
+            openAppStoreIntent.AddFlags(ActivityFlags.NewTask);
+
+            Android.App.Application.Context.StartActivity(openAppStoreIntent);
 
             return Task.CompletedTask;
-        }
-
-        private static ActivityNotFoundException? OpenIntent(string uriIntent)
-        {
-            using var intent = new Intent(Intent.ActionView);
-            intent.SetData(Uri.Parse(uriIntent));
-            intent.AddFlags(ActivityFlags.NewTask);
-            try
-            {
-                Android.App.Application.Context.StartActivity(intent);
-            }
-            catch (ActivityNotFoundException e)
-            {
-                return e;
-            }
-            return null;
         }
     }
 }
