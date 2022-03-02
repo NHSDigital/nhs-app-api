4915d70f0d4c266ace916385f8acbf6a7b551bab
Thomas McDonnell <thomas.mcdonnell1@nhs.net>
2022-03-02 11:01:26 +0000

    NHSO-0000: Adding in check for disposed webview in android

--- xamarin/NHSOnline.App.Android/Renderers/WebViews/Extensions/AccessibilityWebViewRendererExtension.cs
+++ xamarin/NHSOnline.App.Android/Renderers/WebViews/Extensions/AccessibilityWebViewRendererExtension.cs
@@ -1,5 +1,8 @@
+using System;
 using Android.Views.Accessibility;
+using Microsoft.Extensions.Logging;
 using NHSOnline.App.Controls;
+using NHSOnline.App.Logging;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -9,18 +12,27 @@ namespace NHSOnline.App.Droid.Renderers.WebViews.Extensions
     {
         private readonly WebViewRenderer _renderer;
 
+        private static ILogger Logger => NhsAppLogging.CreateLogger(typeof(AccessibilityWebViewRendererExtension));
+
         public AccessibilityWebViewRendererExtension(WebViewRenderer renderer)
             => _renderer = renderer;
 
         internal override void OnElementChanged(ElementChangedEventArgs<WebView> e)
         {
             if (e.OldElement == null &&
-                _renderer.Control.Settings != null &&
+                _renderer.Control?.Settings != null &&
                 _renderer.Element is IAccessibleControl view)
             {
                 view.AccessibilityFocusChangeRequested += (sender, args) =>
                 {
-                    _renderer.Control.SendAccessibilityEvent(EventTypes.ViewAccessibilityFocused);
+                    try
+                    {
+                        _renderer.Control.SendAccessibilityEvent(EventTypes.ViewAccessibilityFocused);
+                    }
+                    catch (ObjectDisposedException e)
+                    {
+                        Logger.LogError(e, "Webview has been exposed and we cannot send the accessibility event");
+                    }
                 };
             }
         }
