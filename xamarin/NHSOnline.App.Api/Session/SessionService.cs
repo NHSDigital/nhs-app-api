36488b75c4d5459497b56acdcc62149266a8d227
Conuir Williamson <conuirw@kainos.com>
2022-03-11 14:52:54 +0000

    NHSO-18487: Retry create session call in certain scenarios

--- xamarin/NHSOnline.App.Api/Session/SessionService.cs
+++ xamarin/NHSOnline.App.Api/Session/SessionService.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -13,6 +18,19 @@ namespace NHSOnline.App.Api.Session
         private readonly ILogger<SessionService> _logger;
         private readonly IApiClientEndpoint<ApiCreateSessionRequest, ApiCreateSessionResult> _createSessionEndpoint;
 
+        private static readonly List<(Type? Exception, Regex Regex)> CreateSessionRetries = new()
+        {
+            (typeof(HttpRequestException), new Regex(".*The Internet connection appears to be offline.*")),
+            (typeof(HttpRequestException), new Regex(".*A server with the specified hostname could not be found.*")),
+            (typeof(HttpRequestException), new Regex(".*bad URL.*")),
+            (typeof(HttpRequestException), new Regex(".*Could not connect to the server.*")),
+            (typeof(HttpRequestException), new Regex(".*An SSL error has occurred and a secure connection to the server cannot be made.*")),
+            (typeof(WebException), new Regex(".*Failed to connect to api.nhsapp.service.nhs.uk.*")),
+            (null, new Regex(".*failed to connect to api.nhsapp.service.nhs.uk.*")),
+            (null, new Regex(".*SSL handshake aborted.*")),
+            (null, new Regex(".*Read error:.*: Failure in SSL library, usually a protocol error.*")),
+        };
+
         public SessionService(
             ILogger<SessionService> logger,
             IApiClientEndpoint<ApiCreateSessionRequest, ApiCreateSessionResult> createSessionEndpoint)
@@ -21,21 +39,8 @@ namespace NHSOnline.App.Api.Session
             _createSessionEndpoint = createSessionEndpoint;
         }
 
-        public async Task<CreateSessionResult> CreateSession(string authCode, string codeVerifier, string referrer, Uri redirectUrl, string integrationReferrer)
-        {
-            try
-            {
-                var request = new ApiCreateSessionRequest(authCode, codeVerifier, referrer, redirectUrl, integrationReferrer);
-                var result = await _createSessionEndpoint.Call(request, CancellationToken.None).ResumeOnThreadPool();
-
-                return result.Accept(this);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "CreateSession Failed");
-                return new CreateSessionResult.Failed();
-            }
-        }
+        public Task<CreateSessionResult> CreateSession(string authCode, string codeVerifier, string referrer, Uri redirectUrl, string integrationReferrer)
+            => CreateSession(authCode, codeVerifier, referrer, redirectUrl, integrationReferrer, true);
 
         public CreateSessionResult Visit(ApiCreateSessionResult.Success success)
         {
@@ -69,5 +74,33 @@ namespace NHSOnline.App.Api.Session
 
         public CreateSessionResult Visit(ApiCreateSessionResult.InternalServerError internalServerError)
             => new CreateSessionResult.InternalServerError(internalServerError.PfsErrorResponse.ServiceDeskReference);
+
+        private async Task<CreateSessionResult> CreateSession(
+            string authCode, string codeVerifier, string referrer,
+            Uri redirectUrl, string integrationReferrer, bool shouldRetry)
+        {
+            try
+            {
+                var request = new ApiCreateSessionRequest(authCode, codeVerifier, referrer, redirectUrl, integrationReferrer);
+                var result = await _createSessionEndpoint.Call(request, CancellationToken.None).ResumeOnThreadPool();
+
+                return result.Accept(this);
+            }
+            catch (Exception e)
+            {
+                if (shouldRetry && ShouldRetryOnException(e))
+                {
+                    _logger.LogError(e, "CreateSession Failed - Retrying");
+                    return await CreateSession(authCode, codeVerifier, referrer, redirectUrl, integrationReferrer, false).ResumeOnThreadPool();
+                }
+
+                _logger.LogError(e, "CreateSession Failed");
+                return new CreateSessionResult.Failed();
+            }
+        }
+
+        private static bool ShouldRetryOnException(Exception exception) =>
+            CreateSessionRetries.Any(retry =>
+                (retry.Exception is null || exception.GetType() == retry.Exception) && retry.Regex.IsMatch(exception.Message));
     }
 }
