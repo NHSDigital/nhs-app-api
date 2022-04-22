50c7fcc1b3319020d4366c20f5395248fe5fd036
Ian Nelson <ian@iannelson.systems>
2022-04-22 07:37:49 +0100

    NHSO-20136: Fix bug where unable to send comms to user with BetaTester

--- backendworker/NHSOnline.Backend.UserInfo/Repository/Info.cs
+++ backendworker/NHSOnline.Backend.UserInfo/Repository/Info.cs
@@ -1,9 +1,14 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace NHSOnline.Backend.UserInfo.Repository
 {
+    [BsonIgnoreExtraElements]
     public class Info
     {
+        [BsonElement]
         public string OdsCode { get; set; }
 
+        [BsonElement]
         public string NhsNumber { get; set; }
     }
 }
