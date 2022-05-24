d152cf6dd88056c32f496356e5c521dbbea84b95
jackryan.walton@kainos.com <JAWA30@hscic.gov.uk>
2022-05-24 15:04:28 +0100

    NHSO-0000: stop int tests searching for error box on new shutter screen

--- xamarinintegrationtests/NHSOnline.IntegrationTests.Pages/WebPageContent/NhsAppWeb/Prescriptions/PrescriptionsTemporaryProblemPageContent.cs
+++ xamarinintegrationtests/NHSOnline.IntegrationTests.Pages/WebPageContent/NhsAppWeb/Prescriptions/PrescriptionsTemporaryProblemPageContent.cs
@@ -12,15 +12,13 @@ namespace NHSOnline.IntegrationTests.Pages.WebPageContent.NhsAppWeb.Prescription
         private WebText TitleText => WebText.WithTagAndText(_interactor,
             "h1", "Sorry, there is a problem getting your repeat prescription information");
 
-        private WebPanel ErrorPanel => WebPanel.WithTitle(_interactor, "Error");
-
-        private WebText UnableToBookText => ErrorPanel.ContainingTagWithText(
+        private WebText UnableToBookText => WebText.WithTagAndText(_interactor,
             "p", "You are not currently able to order or view repeat prescriptions online.");
 
-        private WebText TemporaryProblemText => ErrorPanel.ContainingTagWithText(
+        private WebText TemporaryProblemText => WebText.WithTagAndText(_interactor,
             "p", "This may be a temporary problem.");
 
-        private WebButton TryAgainButton => ErrorPanel.ContainingButtonWithText("Try again");
+        private WebButton TryAgainButton => WebButton.WithText(_interactor, "Try again");
 
         internal void AssertOnPage() => TitleText.AssertVisible();
 
