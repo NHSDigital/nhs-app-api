e801fef3f0b30fcf5a799c7d79446ba9646c21e1
Thomas McDonnell <thomas.mcdonnell1@nhs.net>
2022-06-13 11:59:50 +0100

    NHSO-20966: Removing unknown package for pit app in playstore

--- xamarin/NHSOnline.App.Android/DependencyServices/AndroidUpdateService.cs
+++ xamarin/NHSOnline.App.Android/DependencyServices/AndroidUpdateService.cs
@@ -16,41 +16,29 @@ namespace NHSOnline.App.Droid.DependencyServices
 
         public Task OpenAppStoreUrl()
         {
-            var storeIntentResult = OpenPlayStoreIntent("market://details?id=com.nhs.online.nhsonline");
+            var storeIntentResult = OpenIntent("market://details?id=com.nhs.online.nhsonline");
             if (storeIntentResult == null)
             {
+                Logger.LogInformation("Successfully launched play store store from market url");
                 return Task.CompletedTask;
             }
 
             var browserIntentResult = OpenIntent("https://play.google.com/store/apps/details?id=com.nhs.online.nhsonline");
             if (browserIntentResult == null)
             {
-                Logger.LogError("Failed to open play store intent. {ErrorMessage}", storeIntentResult.Message);
+                Logger.LogInformation("Successfully launched play store store from browser url");
+                return Task.CompletedTask;
             }
-            else
-            {
-                Logger.LogError("Failed to open play store and browser intents. {ErrorMessage}\n{AdditionalErrorMessage}",
+
+            Logger.LogError("Failed to open play store and browser intents. {ErrorMessage}\n{AdditionalErrorMessage}",
                     storeIntentResult.Message, browserIntentResult.Message);
-            }
 
             return Task.CompletedTask;
         }
 
-        private static ActivityNotFoundException? OpenPlayStoreIntent(string uriIntent)
+        private static ActivityNotFoundException? OpenIntent(string uriIntent)
         {
-            // de.androidpit.app is the package name for the Play Store
-            var intent = Android.App.Application.Context.PackageManager?.GetLaunchIntentForPackage("de.androidpit.app");
-            if (intent == null)
-            {
-                return new ActivityNotFoundException("Play store was not found by package name de.androidpit.app");
-            }
-
-            return OpenIntent(uriIntent, intent);
-        }
-
-        private static ActivityNotFoundException? OpenIntent(string uriIntent, Intent? launchIntent = null)
-        {
-            using var intent = launchIntent ?? new Intent(Intent.ActionView);
+            using var intent = new Intent(Intent.ActionView);
             intent.SetData(Uri.Parse(uriIntent));
             intent.AddFlags(ActivityFlags.NewTask);
             try
