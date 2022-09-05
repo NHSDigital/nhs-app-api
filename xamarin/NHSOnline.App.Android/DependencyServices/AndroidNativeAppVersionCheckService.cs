69120aadb4c52c1148a7049ed629b34016e2e936
Liam Mullen <liamm@kainos.com>
2022-09-05 10:32:30 +0100

    NHSO-22123: Adding more logging to current App version check

--- xamarin/NHSOnline.App.Android/DependencyServices/AndroidNativeAppVersionCheckService.cs
+++ xamarin/NHSOnline.App.Android/DependencyServices/AndroidNativeAppVersionCheckService.cs
@@ -1,9 +1,11 @@
+using System;
+using Android.Content;
+using Android.Content.PM;
 using Microsoft.Extensions.Logging;
 using NHSOnline.App.Api.Configuration;
 using NHSOnline.App.DependencyServices;
 using NHSOnline.App.Droid.DependencyServices;
 using NHSOnline.App.Logging;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(AndroidNativeAppVersionCheckService))]
@@ -15,12 +17,31 @@ namespace NHSOnline.App.Droid.DependencyServices
 
         public bool MeetsMinimumVersion(VersionConfiguration versionConfiguration)
         {
-            var currentVersion = AppInfo.Version;
+            Version currentVersion = new (GetPackageVersion() ??
+                                 throw new ArgumentNullException("Current Version String is null", nameof(currentVersion)));
+
             var minimumSupportedVersion = versionConfiguration.MinimumSupportedAndroidVersion;
 
             Logger.LogInformation( $"Update Required Check. Current Version: {currentVersion}. Minimum Version: {minimumSupportedVersion}.");
 
             return currentVersion >= minimumSupportedVersion;
+        }
+
+        /// <summary>
+        /// Using this as opposed to AppInfo.Version due to Xamarin Essentials not being thread safe
+        /// https://github.com/xamarin/Essentials/issues/1959
+        /// </summary>
+        /// <returns></returns>
+        private static string? GetPackageVersion()
+        {
+            Context context = Android.App.Application.Context;
+            PackageManager? manager = context.PackageManager;
+            if (context.PackageName != null && manager != null)
+            {
+                var info = manager.GetPackageInfo(context.PackageName, 0);
+                return info?.VersionName;
+            }
+            return null;
         }
     }
 }
